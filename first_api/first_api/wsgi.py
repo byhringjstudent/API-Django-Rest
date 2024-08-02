1cf942c9aaa616f0e2d5c371de672bfce3af5977
byhringjstudent <byhringj@dupage.edu>
2024-08-02 16:35:29 -0500

    final commit

--- first_api/first_api/wsgi.py
+++ first_api/first_api/wsgi.py
@@ -1,11 +1,6 @@
-"""
-WSGI config for first_api project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+###It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
 
 import os
 
