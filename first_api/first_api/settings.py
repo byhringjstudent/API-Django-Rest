808c052c37e97d91abdd07b13ce5be5403fa136f
byhringjstudent <byhringj@dupage.edu>
2024-08-02 14:15:15 -0500

    latest commit

--- first_api/first_api/settings.py
+++ first_api/first_api/settings.py
@@ -1,22 +1,12 @@
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-tg6m*^47ufy2@^1lm+-u5yc)cb16mwqce0d=1p_o78ff9xi#ww"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
